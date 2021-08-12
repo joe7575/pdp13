ed469ae23a3da8cc2bf31d3b57c951d2ead45984
Joachim Stolberg <joe.stolberg@gmx.de>
2021-08-12 21:09:43 +0200

    Add the Englisch manual

--- manuals/manual_EN.lua
+++ manuals/manual_EN.lua
@@ -1,20 +1,22 @@
 techage.add_to_manual('EN', {
-  "1,PDP13 Minicomputer (TA3)",
-  "2,Instructions",
+  "1,PDP13 minicomputer (TA3)",
 }, {
-  "A 16-bit minicomputer simulation inspired by DEC\\, IBM\\, and other\n"..
-  "Vintage Computers from the 60s and 70s.\n"..
+  "PDP13 is a 16-bit minicomputer inspired by DEC\\, IBM\\, and other computers from the 1960s and 1970s. \"Mini\" because up until then the computer systems had not only filled cupboards\\, but entire rooms or halls. It was only with the invention of the first integrated circuits that computers could be reduced to the size of a wardrobe. This makes this computer ideal for the age of oil. Because this computer can only be programmed in machine code (like the originals at the time)\\, this requires a certain amount of computer knowledge\\, which cannot be conveyed in this manual.\n"..
+  "\n"..
+  "The prerequisites are:\n"..
+  "\n"..
+  "  - Calculating with HEX numbers (16 bit system)\n"..
+  "  - Basic knowledge of the structure of a CPU (register\\, memory addressing) and assembler programming\n"..
+  "  - Perseverance and willingness to learn\\, because PDP13 is different than anything you may already know\n"..
+  "\n"..
+  "The PDP13 minicomputer is not needed in the game\\, but rather serves as teaching material in computer basics and computer history. However\\, like the other controllers\\, it can be used to control machines. The PDP13 Mod also has its own output blocks\\, so that there are many possibilities for use.\n"..
+  "\n"..
+  "Due to the length of the manual\\, it is not available in-game. However\\, it is advisable to read these instructions directly on GitHub or to download them from there. The link 'github.com/joe7575/pdp13/wiki' takes you directly to the page with instructions and further links.\n"..
   "\n"..
   "\n"..
-  "\n",
-  "Unfortunately not avaiable :=(\n"..
-  "\n"..
-  "See German manual instead\n"..
   "\n",
 }, {
   "pdp13_cpu",
-  "",
 }, {
-  "",
   "",
 })
